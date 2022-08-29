6bde8a4ec7dc744534c1696c06ef16fa4db79817
Derix10 <sejitbekovarsen12.@gmail.com>
2022-08-29 16:48:27 +0600

    comtitt

--- app/src/main/java/com/example/derixfox/MainActivity.java
+++ app/src/main/java/com/example/derixfox/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.derixfox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MaterialButton text444;
+    private EditText text222, text333;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text444 = findViewById(R.id.text444);
+        text333 = findViewById(R.id.text333);
+        text222 = findViewById(R.id.text222);
+
+
     }
+
 }
