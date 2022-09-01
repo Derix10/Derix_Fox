8c1e7520b17ce00b07378ca8aaf72b687a66ea3c
Derix10 <sejitbekovarsen12.@gmail.com>
2022-09-01 22:22:23 +0600

    comtit

--- app/src/main/java/com/example/derixfox/MainActivity.java
+++ app/src/main/java/com/example/derixfox/MainActivity.java
@@ -2,24 +2,104 @@ package com.example.derixfox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.EdgeEffect;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    private MaterialButton text444;
-    private EditText text222, text333;
+    private MaterialButton button_one;
+    private TextView text111, text555, text666, textView,text000;
+
+
+    private EditText editText1, editText2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text444 = findViewById(R.id.text444);
-        text333 = findViewById(R.id.text333);
-        text222 = findViewById(R.id.text222);
+        button_one = findViewById(R.id.text444);
+        editText2 = findViewById(R.id.text333);
+        editText1 = findViewById(R.id.text222);
+        text555 = findViewById(R.id.text555);
+        text666 = findViewById(R.id.text666);
+        textView = findViewById(R.id.textView);
+        text111 = findViewById(R.id.text111);
+        text000 = findViewById(R.id.text000);
 
 
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editText1) {
+                if (editText1.toString().length() > 0) {
+                    button_one.setBackgroundColor(Color.RED);
+                } else {
+                    button_one.setBackgroundColor(Color.GRAY);
+                }
+            }
+        });
+
+        editText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editText2) {
+                if (editText2.toString().length() > 0) {
+                    button_one.setBackgroundColor(Color.RED);
+                } else {
+                    button_one.setBackgroundColor(Color.GRAY);
+                }
+            }
+        });
+
+
+        button_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText1.getText().toString().equals("admin") && editText2.getText().toString()
+                        .equals("admin")) {
+                    text000.setVisibility(View.GONE);
+                    text111.setVisibility(View.GONE);
+                    editText1.setVisibility(View.GONE);
+                    editText2.setVisibility(View.GONE);
+                    button_one.setVisibility(View.GONE);
+                    text555.setVisibility(View.GONE);
+                    text666.setVisibility(View.GONE);
+                    Toast.makeText(MainActivity.this, "Вы успешно зарегистрировались"
+                            , Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Неправильный пароль или Email",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
 
 }
